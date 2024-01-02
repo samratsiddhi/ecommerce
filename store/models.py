87830e650ba1f6f76b884d6d8e1c7cd1562cf92c
Samrat Bajracharya <siddhibro@gmail.com>
2024-01-02 15:15:01 +0545

    Cart Item added to db

--- store/models.py
+++ store/models.py
@@ -46,9 +46,17 @@ class Cart(models.Model):
     
     def __str__(self):
         return self.id
+  
+  
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete =  models.CASCADE)
+    quantity = models.IntegerField(default = 1)
     
+    
+    
+    def __str__(self):
+        return self.id
 
-    
     
 
     
