32227a5f67ea080487d0098d39f1d3ebef37839f
Samrat Bajracharya <siddhibro@gmail.com>
2024-01-02 14:21:05 +0545

    added category and product to db

--- store/models.py
+++ store/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    quanity = models.IntegerField(default=0)
+    price = models.FloatField()
+    discounted_price = models.FloatField()
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+
