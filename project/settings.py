9a9d33732c53bc2d4b0b4e834b1a9cace15aa84b
Samrat Bajracharya <siddhibro@gmail.com>
2024-01-02 13:50:19 +0545

    connected to mysql

--- project/settings.py
+++ project/settings.py
@@ -79,12 +79,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
+        'NAME': 'ecommerce',
         'USER': 'root',
-        'PASSWORD': 'admin',
+        'PASSWORD': '',
         'HOST':'localhost',
         'PORT':'3306',
     }
