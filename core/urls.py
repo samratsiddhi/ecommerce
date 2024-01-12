f64abe07a02c999f348e880655c20a88d9abeef3
Samrat Bajracharya <siddhibro@gmail.com>
2024-01-12 13:12:23 +0545

    user login with token generation and user registration

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('login',login)
+    path('login',login),
+    path('register',register)
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate,get_user_model
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from django.contrib.auth.hashers import make_password
+
 
 
 # Create your views here.
@@ -21,3 +23,29 @@ def login(request):
         })
     
     return Response("invalid")
+
+@api_view(['POST'])
+def register(request):
+
+    username = request.data.get('username')
+    password = request.data.get('password')
+    confirm_password = request.data.get('confirm_password')
+    
+    
+    User = get_user_model()
+    
+    if User.objects.filter(username=username).exists():
+        return Response(f"User already exists")
+
+    if password == confirm_password:
+        
+        user = User(username=username)
+        # user.set_password(password)
+        user.password = make_password(password)
+        
+        user.save()
+        
+        return Response({
+            'detail' : 'user reregister success'
+        })
+    return Response('Password does not match')
